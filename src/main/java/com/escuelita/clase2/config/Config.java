cea0b263182f7125ed3bcf4810e27ef5fd9db111
robby moyano <robby.moyano.toledo@accenture.com>
2020-11-17 12:04:04 -0300

    Config para port Soap

--- src/main/java/com/escuelita/clase2/config/Config.java
+++ src/main/java/com/escuelita/clase2/config/Config.java
@@ -1,24 +1,54 @@
 package com.escuelita.clase2.config;
 
+import javax.xml.ws.BindingProvider;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import org.tempuri.Calculator;
+import org.tempuri.CalculatorSoap;
+
+import com.sun.xml.ws.client.BindingProviderProperties;
 
 @Configuration
 public class Config {
 
 	@Value("${client.rest.timeout}")
 	int timeout;
-	
+
 	public RestTemplate getClientRest() {
 		SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
 		factory.setConnectTimeout(timeout);
 		factory.setReadTimeout(timeout);
-		
+
 		RestTemplate restTemplate = new RestTemplate();
 		restTemplate.setRequestFactory(factory);
 		return restTemplate;
+
+	}
+
+	@Bean
+	public CalculatorSoap getSoapClient() {
+		String endpoint = "http://www.dneonline.com/calculator.asmx?wsdl";
+		int timeOut = 6000;
+
+		int requestTimeout = timeOut;
+		int connectTimeout = timeOut;
 		
+		Calculator servicio = new Calculator();
+		CalculatorSoap port = servicio.getCalculatorSoap();
+
+		// Configuración para el redireccionamiento
+		BindingProvider bindingProvider = (BindingProvider) port;
+		bindingProvider.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, endpoint);
+
+		// Configuración para el timeout
+		bindingProvider.getRequestContext().put(BindingProviderProperties.REQUEST_TIMEOUT, requestTimeout);
+		bindingProvider.getRequestContext().put(BindingProviderProperties.CONNECT_TIMEOUT, connectTimeout);
+		
+		return port;
+
 	}
 }
--- src/main/java/com/escuelita/clase2/service/ClienteSoap.java
+++ src/main/java/com/escuelita/clase2/service/ClienteSoap.java
@@ -2,21 +2,26 @@ package com.escuelita.clase2.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.tempuri.Add;
 import org.tempuri.AddResponse;
 import org.tempuri.Calculator;
 import org.tempuri.CalculatorSoap;
 
+import com.escuelita.clase2.config.Config;
+
 @Service
 public class ClienteSoap {
 	private static final Logger log = LoggerFactory.getLogger(ClienteSoap.class);
 	
+	@Autowired
+	Config config;
+	
 	public void callSoap() {
 		log.info("Inicio SOAP");
 		
-		Calculator calculadora = new Calculator();
-		CalculatorSoap port = calculadora.getCalculatorSoap();
+		CalculatorSoap port = config.getSoapClient();
 		
 		Add suma = new Add();
 		suma.setIntA(40);
